c9fd783ac9d955881fde7a7a857ac8db0e4e9cb1
Ryan Krattiger <ryan.krattiger1234@gmail.com>
2015-12-12 15:07:52 -0600

    fixed file path

--- GenerateDogBoneMesh.m
+++ GenerateDogBoneMesh.m
@@ -1,7 +1,7 @@
 function [meshNodes,meshElems] = GenerateDogBoneMesh(w,h,L,r,meshSize)
 
-addpath(genpath('distMesh'));
-addpath(genpath('postProcessing'));
+addpath(genpath('distmesh'));
+addpath(genpath('postprocessing'));
 
 fd=@(p) ddiff(ddiff(ddiff(ddiff(dunion(dunion(dunion(dunion(drectangle(p,-w,w,0,h),drectangle(p,-w+r,w-r,h,h+r)),drectangle(p,-w+2*r,w-2*r,h,h+L)),drectangle(p,-w+r,w-r,h+L-r,h+L)),drectangle(p,-w,w,h+L,2*h+L)),dcircle(p,-w+r,h+r,r)),dcircle(p,w-r,h+r,r)),dcircle(p,w-r,h+L-r,r)),dcircle(p,-w+r,h+L-r,r));
 
